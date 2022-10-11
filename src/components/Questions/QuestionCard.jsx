1ecd724c4c0c0a1e5925c3ce11361e2c9cdc8e28
Upal Barua <upalbarua52@gmail.com>
2022-10-11 10:45:53 +0600

    added correct answer button and answer input validation

--- src/components/Questions/QuestionCard.jsx
+++ src/components/Questions/QuestionCard.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import parse from 'html-react-parser';
+import { AiOutlineEye } from 'react-icons/ai';
 import styles from './QuestionCard.module.css';
 
 const QuestionCard = ({ questionData }) => {
@@ -15,9 +16,21 @@ const QuestionCard = ({ questionData }) => {
 
   const answerSubmitHandler = event => {
     event.preventDefault();
+
+    if (!selectedAnswer.length) {
+      alert('no answer selected');
+      return;
+    }
+
     setIsAnswerCorrect(prevIsAnswerCorrect => {
       return (prevIsAnswerCorrect = selectedAnswer === correctAnswer);
     });
+
+    console.log(`Your answer is ${isAnswerCorrect ? 'correct' : 'wrong'}`);
+  };
+
+  const correctAnswerHandler = () => {
+    console.log(`The correct answer is ${correctAnswer}`);
   };
 
   return (
@@ -26,9 +39,11 @@ const QuestionCard = ({ questionData }) => {
       style={{
         backgroundColor: 'var(--clr-dark-500)',
         backgroundColor:
-          isAnswerCorrect !== null && (isAnswerCorrect ? 'lime' : 'red'),
+          isAnswerCorrect !== null &&
+          (isAnswerCorrect ? 'var(--clr-green-400)' : 'var(--clr-red-400)'),
       }}>
       {parse(question)}
+
       <form className={styles.optionsContainer} onSubmit={answerSubmitHandler}>
         {options.map(option => (
           // NEED TO CHECK THIS PART AND TRY TO CHANGE THE KEY
@@ -43,9 +58,18 @@ const QuestionCard = ({ questionData }) => {
             <label htmlFor={id}>{option}</label>
           </div>
         ))}
-        <button className={`${styles.submitBtn} btn`} type="submit">
-          submit
-        </button>
+
+        <div className={styles.btnGroup}>
+          <button className={`${styles.submitBtn} btn`} type="submit">
+            submit
+          </button>
+          <button
+            className={`${styles.answerBtn} btn`}
+            type="button"
+            onClick={correctAnswerHandler}>
+            <AiOutlineEye />
+          </button>
+        </div>
       </form>
     </div>
   );
