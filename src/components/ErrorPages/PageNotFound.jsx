d03a93cf95c52f67fa1af9d9b16241032542369e
Upal Barua <upalbarua52@gmail.com>
2022-10-11 19:46:06 +0600

    Tweaked the 404 page

--- src/components/ErrorPages/PageNotFound.jsx
+++ src/components/ErrorPages/PageNotFound.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './PageNotFound.module.css';
+import '../../Utils.module.css';
+import { BiMessageSquareError } from 'react-icons/bi';
 
 const PageNotFound = () => {
-  return <div>404 PageNotFound</div>;
+  return (
+    <div className={`${styles.errorPage} container`}>
+      <h2 className={styles.title}>
+        <span>404!</span>
+        <BiMessageSquareError />
+      </h2>
+      <p className={styles.text}>Oops! Page Not Found</p>
+      <p className={styles.message}>
+        Sorry, the page you requested could not be found
+      </p>
+      <Link className={`${styles.homeBtn} btn`} to="/">
+        Go Home
+      </Link>
+    </div>
+  );
 };
 
 export default PageNotFound;
